a4d560fc90016549de7b134bcd2150bc76e33341
Amnok <ankitsingh80741@gmail.com>
2022-04-25 21:46:30 +0530

    binary-search

---
+++ searching/binary-search-recursive.js
@@ -0,0 +1,22 @@
+
+let ar = [ 2, 3, 4, 10, 40 ];
+let x = 10;
+let l = 0;
+let r = ar.length - 1;
+
+function binarySearch(arr, val, left, right) {
+    // if()
+    if(left > right ) return null;
+let mid = Math.floor((left + right) / 2);
+console.log({left, right, mid});
+if(val === arr[mid]) return mid;
+if(val > arr[mid]) {
+    return binarySearch(arr, val, mid+ 1, right );
+} 
+if (val < arr[mid]) {
+    return binarySearch(arr, val, left, mid-1 );
+}
+}
+
+let t = binarySearch(ar, x, l, r);
+console.log(t);
