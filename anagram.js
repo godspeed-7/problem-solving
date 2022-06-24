19023a719a08ac15e874cf8ff0ac46b90d7975a4
Amnok <ankitsingh80741@gmail.com>
2022-06-24 15:53:10 +0530

    anagram first type

--- anagram.js
+++ anagram.js
@@ -1,10 +1,12 @@
+// solution 1
 function isAnagram(string1, string2) {
 if(!(string1 && string2)) return false;
 const upperstr1 = string1.split('').sort().join('').toUpperCase();
 const upperstr2 = string2.split('').sort().join('').toUpperCase();
 return upperstr1 === upperstr2;
 }
-
  console.log(isAnagram('racecar', 'carrace'))
-
 console.log(isAnagram('racecar', 'carracr'))
+
+// solution 2 (without any util methods)
+
