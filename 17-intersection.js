10a193d9f4ceef86903c4116a4af62d2e2ea1f07
Amnok <ankitsingh80741@gmail.com>
2022-06-28 20:31:08 +0530

    intersection problem added

---
+++ 17-intersection.js
@@ -0,0 +1,31 @@
+const array1 = ['1', '2', '3', '4'];
+const array2 = ['4', '5','6', '7', '8', '10'];
+['4']
+const array3 = ['21', '22', '23', '24'];
+const array4 = ['24', '25','26', '27', '28', '29'];
+
+['24']
+
+['']
+
+function intersection(array1, array2) {
+const set1 = new Set(array1);
+const set2 = new Set(array2);
+const set3 = new Set();
+for (let i = 0  of array1) {
+//
+}
+return set3;
+}
+
+
+function calculateIntersection(n, arrays) {
+const intersections = [];
+for(i  = 0; i<n ;i+2) {
+	const intersectin = intersection(arrays[i], arrays[i+1]);
+  intersections.push(intersection);
+}
+ intersections.length > 1 ?  calculateIntersection(intersections.length, intersections) : intersections;
+}
+
+calculateIntersection(n, arrays)
